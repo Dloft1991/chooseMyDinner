fa8430141041a5ecb3169070cf1c87ac6c965e6f
Derek Lofton Work <derekloftonwork@Dereks-MacBook-Pro.local>
2021-03-02 22:20:27 -0500

    mobiledone

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const restraunts = ["Arby's", "McDonalds", "Chic-fil-A", "Chipotle", "Five Guys"];
+const restraunts = ["Arby's", "McDonalds", "Chic-fil-A", "Chipotle", "Five Guys", "Zaxby's", "Publix", "Pizza", "Cheddars",
+"Burger King", "Panda Express", "Firehouse Subs", "Kobe's", "Cheddars", "Bahama Breeze", "Ale House", "King's Wok"];
 
 RandomChoice = () => {
     const random = Math.floor(Math.random() * restraunts.length);
 console.log(random, restraunts[random]);
 document.getElementById("TheChoice").innerHTML = "How about" + " " + restraunts[random] + "?";
 }
-
 
